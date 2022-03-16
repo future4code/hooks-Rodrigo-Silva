a0d4eda1e2b52cb8a1960d9788a580f546313d30
RodrigoOtavioSilva <rod.otavio@hotmail.com>
2022-03-15 23:48:08 -0300

    Resolvendo Problemas

---
+++ modulo1/resolvendo-problemas/index.js
@@ -0,0 +1,51 @@
+/* Exercícios de fixação do conhecimento
+
+A resolução de problemas é essencial para todos os trabalhos, não só para o qual estamos estudando agora. 
+Realmente, os 7 tópicos trabalhados em aula podem (ou até deveriam) ser usados em qualquer emprego para qualquer 
+situação mais “cabeluda” que aparecesse no dia-a-dia. Esse é um tópico muito interessante que precisa ser 
+trabalhado tanto quanto o estudo do código em si. As vezes pensando calmamente e seguindo os passos, a solução 
+vai ser mais simples do que parece ser. O mundo da programação é vasto e complexo, com vários parâmetros e caminhos 
+que podemos seguir para chegar ao mesmo destino, e quando encontramos um problema podemos não sair do lugar, pensando 
+que a solução é complexa, dar um passo para trás e analisar é um passo essencial, que me atrevo a dizer que em 90% dos 
+casos será preciso fazer.
+
+Agora explicando os 7 passos estudados, em resumo:
+
+Passo 1 - Ter Atenção no Enunciado
+As vezes na animação de criar algo novo, ou querendo cumprir um prazo, ou apenas por desatenção, interpretamos 
+erroneamente o que está sendo pedido. A primeira coisa que deve ser feito é ler com extrema atenção todo o conteúdo 
+que está sendo pedido, em seguida encontrar qual é a finalidade do que você está criando, o porquê esse trabalho está sendo feito. 
+Encontrando o objetivo do enunciado, é importante trabalhar os caminhos que podem ser seguidos, mantendo somente aquilo que seja 
+essencial nas informações entregues, que não podem ser mudadas.
+
+Passo 2 - Retirar Informações Relevantes
+As pessoas são diferentes umas das outras, temos ideias diferentes e jeitos de trabalhar diferentes, mas em programação outras pessoas 
+podem ter acesso ao que escrevemos, então um código confuso pode ser bastante prejudicial no futuro. Tente encontrar palavras chaves que 
+sejam fáceis de encontrar ou lembrar depois, principalmente para as variáveis. Para funções é comum usar verbos mais diretos, que facilitam 
+a indicação do que estão fazendo. E colocar só o importante, evite colocar códigos desnecessários.
+
+Passo 3 - Pensar nas Entradas e Saídas
+O código precisa receber informações, então busque descobrir quais são os dados que serão recebidos e como eles devem ser recebidos, 
+cada caso é um caso que deve ser analisado. Também pensar quais vão ser as saídas e de qual forma será, para facilitar o código em si. 
+Funções são importantes e essenciais, pode ser confuso mas facilita a forma que escrevemos códigos grandes, para não ficar repetitivo e
+ cansativo.
+
+Passo 4 - Fazer Um Esquema Visual
+A visualização mental do que está sendo pedido e visualizar é uma ótima ferramenta. Tente criar exemplos e testar vários jeitos, monte 
+gráficos e diagramas (pode ser até com papel e caneta mesmo) para conseguir chegar em um resultado mais assertivo. E tente pensar e colocar 
+tudo nesses exemplos, tenta montar o cenário ideal e nas formas de ir melhorando.
+
+Passo 5 - Criar Um Código Feio
+Apesar do que disse sobre tentar sem mais objetivo e menos repetitivo, inicialmente crie da sua forma, com quantas soluções possíveis 
+de você lembrar, e depois vá corrigindo aos poucos. E o principal, teste quantas vezes forem necessárias, até se sentir satisfeito.
+
+Passo 6 - Testar: Força-Bruta
+Como disse antes, não tenha medo de testar, teste e erre quantas vezes forem necessárias. Escreva um texto mesmo com o que 
+você quer e depois transcreva no código, vai ser uma mão na roda.
+
+Passo 7 -  Refatorar
+Seja organizado. Dê nomes fácies de se lembrar e de até escrever as variáveis e funções, não de nomes gigantes ou com escritas 
+excêntricas. Separe as funções de cada parte do código, para cada resultado que você quer. Vá por etapas que não tem erro.
+
+
+*/
